2a8cf57973e7796adb411584d71289d2250af749
as2pick <vooxify.public@outlook.fr>
2025-02-15 12:04:41 +0100

    component

--- src/components/RoundedBox.jsx
+++ src/components/RoundedBox.jsx
@@ -4,7 +4,7 @@ const {
     screen: { height, width },
 } = GLOBALS_DATAS;
 
-export default function RoundedBox({ course }) {
+export default function RoundedBox({ course, boxColor }) {
     const {
         classGroup,
         endCourse,
@@ -20,12 +20,24 @@ export default function RoundedBox({ course }) {
     } = course;
 
     return (
-        <View style={[styles.box, isCancelled && styles.cancelled]}>
+        <View
+            style={[
+                styles.box,
+                isCancelled && styles.cancelled,
+                {
+                    borderColor: boxColor,
+                    backgroundColor: "rgb(242, 242, 242)",
+                    width: width,
+                },
+            ]}
+        >
             <View style={styles.content}>
-                <Text style={styles.title}>{libelle}</Text>
+                <Text style={[styles.title, { backgroundColor: boxColor }]}>
+                    {libelle}
+                </Text>
                 <Text style={styles.teacher}>{teacher}</Text>
+                <Text style={styles.room}>{room}</Text>
             </View>
-            <Text style={styles.room}>{room}</Text>
             {isCancelled && <Text style={styles.cancelledText}>Cancelled</Text>}
         </View>
     );
@@ -33,30 +45,43 @@ export default function RoundedBox({ course }) {
 
 const styles = StyleSheet.create({
     box: {
-        backgroundColor: "rgb(230, 230, 230)",
-        padding: 16,
+        padding: 7,
+        marginVertical: 10,
         borderRadius: 10,
-        borderWidth: 0.8,
-        borderColor: "rgb(212, 212, 212)",
-        elevation: 10,
+
+        elevation: 6,
         flexDirection: "column",
         justifyContent: "space-between",
         height: 70,
+        position: "relative",
     },
     content: {
         flex: 1,
+
+        position: "relative",
     },
     title: {
         fontSize: 18,
+        position: "absolute",
+        borderRadius: 6,
+        padding: 4,
+        top: 0,
+        left: 0,
     },
     teacher: {
+        bottom: 0,
+        right: 0,
+        position: "absolute",
         fontSize: 16,
         color: "gray",
     },
     room: {
+        position: "absolute",
+        top: 0,
+        right: 0,
         fontSize: 14,
+        fontWeight: "bold",
         color: "darkgray",
-        alignSelf: "flex-end",
     },
     cancelled: {
         backgroundColor: "rgb(255, 230, 230)",
