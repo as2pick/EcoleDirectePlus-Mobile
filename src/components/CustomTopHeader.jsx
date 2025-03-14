3528a640cdfa76e8534d9e2a2a5e6685cf4fc5ab
as2pick <vooxify.public@outlook.fr>
2025-03-14 20:23:49 +0100

    create(custom top header component): To customise and edit perfectly the top bar

---
+++ src/components/CustomTopHeader.jsx
@@ -0,0 +1,50 @@
+import { useNavigation, useTheme } from "@react-navigation/native";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import BackArrow from "../assets/svg/BackArrow";
+
+export default function CustomTopHeader({
+    headerTitle,
+    backArrow = { color: "white", size: 24 },
+}) {
+    const navigation = useNavigation();
+    const { colors } = useTheme();
+    const Title = ({ customStyle = {}, children }) => {
+        return (
+            <Text style={[styles.title, customStyle, { color: colors.txt.txt1 }]}>
+                {children}
+            </Text>
+        );
+    };
+
+    return (
+        <SafeAreaView style={styles.header}>
+            <TouchableOpacity
+                style={styles.backArrow}
+                onPress={() => navigation.goBack()}
+            >
+                <BackArrow fill={backArrow.color} size={backArrow.size} />
+                <Title customStyle={styles.headerTitle}>{headerTitle}</Title>
+            </TouchableOpacity>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 18,
+        fontWeight: 600,
+        marginLeft: 20,
+    },
+    header: {
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        paddingVertical: 10,
+        marginHorizontal: 20,
+        position: "relative",
+    },
+    backArrow: {
+        flexDirection: "row",
+    },
+});
+
