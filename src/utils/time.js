8c6ecd2c351cd37bd0fc8f1f7e315cd7b8e161f7
as2pick <vooxify.public@outlook.fr>
2025-03-15 16:58:07 +0100

    add(getTimeInterval): and remove useless comment

--- src/utils/time.js
+++ src/utils/time.js
@@ -9,3 +9,45 @@ export const toHoursMinutes = (ms) => {
     return [hours, minutes];
 };
 
+export const getTimeInterval = (
+    textInitDate /* YYYY-MM-DDTHH:MM */,
+    textDestDate /* YYYY-MM-DDTHH:MM */
+) => {
+    // CONVERT TO MINUTES !
+
+    const [initDate, initTime] = textInitDate.split("T");
+    const [destDate, destTime] = textDestDate.split("T");
+
+    const [initYYYY, initMM, initDD] = initDate.split("-").map(Number);
+    const [destYYYY, destMM, destDD] = destDate.split("-").map(Number);
+
+    const [iDateYYYY, iDateMM, iDateDD] = [
+        destYYYY - initYYYY,
+        destMM - initMM,
+        destDD - initDD,
+    ];
+
+    const dateInterval = (iDateYYYY + iDateMM + iDateDD) * 1440; // mn
+
+    const [initHH, initMm] = initTime.split(":").map(Number);
+    const [destHH, destMm] = destTime.split(":").map(Number);
+
+    const [iTimeHH, iTimeMm] = [destHH - initHH, destMm - initMm];
+
+    const timeInterval = iTimeHH * 60 + iTimeMm;
+    const interval = dateInterval + timeInterval;
+
+    let [intervalDays, intervalHours, intervalMinutes] = [
+        0,
+        Math.floor(interval / 60),
+        interval % 60,
+    ];
+
+    if (Math.abs(intervalHours) >= 24) {
+        let days = Math.round(intervalHours / 24);
+        intervalDays = days;
+    }
+
+    return [intervalDays, intervalHours, intervalMinutes];
+};
+
