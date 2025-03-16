145252207bb2a362ed9331d46065db42c2b61273
as2pick <vooxify.public@outlook.fr>
2025-03-16 12:09:11 +0100

    edit()

--- src/components/UI/Separation.jsx
+++ src/components/UI/Separation.jsx
@@ -1,54 +1,17 @@
-import { StyleSheet, View } from "react-native";
+import { View } from "react-native";
+import { UiStyles } from "./UiStyles";
 
-export const Separation = () => {
+export default function Separation() {
     const { colors } = useTheme();
     return (
-        <View style={styles.separationParent}>
+        <View style={UiStyles.separationParent}>
             <View
                 style={[
-                    styles.separationChildren,
+                    UiStyles.separationChildren,
                     { backgroundColor: colors.txt.txt1 },
                 ]}
             />
         </View>
     );
-};
-const styles = StyleSheet.create({
-    title: {
-        fontSize: 26,
-        fontWeight: 600,
-        marginLeft: 20,
-    },
-    scrollview: {
-        paddingBottom: 32,
-    },
-    subtitle: {
-        fontWeight: 600,
-        fontSize: 18,
-        marginLeft: 20,
-    },
-    paragraph: {
-        marginLeft: 30,
-        marginRight: 20,
-        marginVertical: 4,
-    },
-    separationParent: {
-        alignItems: "center",
-        marginVertical: 14,
-    },
-    separationChildren: {
-        width: "92%",
-        height: 1.8,
-        borderRadius: 999,
-        marginLeft: 30,
-        marginRight: 20,
-    },
-    header: {
-        flexDirection: "row",
-        justifyContent: "center",
-        paddingVertical: 10,
-        marginHorizontal: 5,
-        position: "relative",
-    },
-});
+}
 
