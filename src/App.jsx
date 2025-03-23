3608cc3b2e49732e7dceaf8b540923d9ccb14ca1
as2pick <vooxify.public@outlook.fr>
2025-03-23 09:39:28 +0100

    clean providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,11 @@
-import { Platform, StatusBar, View } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { AppSettingsProvider } from "./context/AppSettingsContext";
-import { GlobalAppProvider } from "./context/GlobalAppContext";
-import { SignInProvider } from "./context/SignInContext";
-import { UserProvider } from "./context/UserContext";
+import RootProviders from "./providers";
 import AuthNavigator from "./router/AuthNavigator";
 
 export default function App() {
     return (
-        <SafeAreaProvider>
-            <GlobalAppProvider>
-                <AppSettingsProvider>
-                    <UserProvider>
-                        <SignInProvider>
-                            <View style={{ flex: 1 }}>
-                                {Platform.OS === "android" && (
-                                    <StatusBar
-                                        backgroundColor={"transparent"}
-                                        translucent={true}
-                                    />
-                                )}
-                                <AuthNavigator />
-                            </View>
-                        </SignInProvider>
-                    </UserProvider>
-                </AppSettingsProvider>
-            </GlobalAppProvider>
-        </SafeAreaProvider>
+        <RootProviders>
+            <AuthNavigator />
+        </RootProviders>
     );
 }
 
---
+++ src/providers/index.jsx
@@ -0,0 +1,31 @@
+import { Platform, StatusBar, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { AppSettingsProvider } from "../context/AppSettingsContext";
+import { GlobalAppProvider } from "../context/GlobalAppContext";
+import { SignInProvider } from "../context/SignInContext";
+import { UserProvider } from "../context/UserContext";
+
+export default function RootProviders({ children }) {
+    return (
+        <SafeAreaProvider>
+            <GlobalAppProvider>
+                <AppSettingsProvider>
+                    <UserProvider>
+                        <SignInProvider>
+                            <View style={{ flex: 1 }}>
+                                {Platform.OS === "android" && (
+                                    <StatusBar
+                                        backgroundColor="transparent"
+                                        translucent={true}
+                                    />
+                                )}
+                                {children}
+                            </View>
+                        </SignInProvider>
+                    </UserProvider>
+                </AppSettingsProvider>
+            </GlobalAppProvider>
+        </SafeAreaProvider>
+    );
+}
+
