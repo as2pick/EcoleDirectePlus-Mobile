a1d3c97f6c9c19a75e79ceb68ca92b37596654f9
Lostosword <alexandrecarenco@gmail.com>
2025-03-29 00:23:52 +0100

    add light (unfinished)

--- src/themes/themes.js
+++ src/themes/themes.js
@@ -5,6 +5,35 @@ export const THEMES = {
     opulent: {
         // light
         ...DefaultTheme,
+        /*
+        colors: {
+            ...DefaultTheme.colors,
+            background: "rgb(235, 225, 255)",
+            bg: {
+                bg1: "rgb(220, 200, 255)",
+                bg2: "rgb(210, 190, 245)",
+                bg3: "rgb(200, 180, 235)",
+                bg4: "rgb(190, 170, 225)",
+                bg5: "rgb(180, 160, 215)",
+                bg6: "rgb(170, 150, 205)",
+            },
+            txt: {
+                txt1: "rgb(40, 20, 80)",
+                txt2: "rgb(70, 50, 120)",
+                txt3: "rgb(100, 80, 160)",
+            },
+            navbar: {
+                icons: "rgb(40, 20, 80)",
+                active_icon_bg: "rgb(210, 190, 245)",
+                icons_bg: "rgb(220, 200, 255)",
+                background: "rgb(235, 225, 255)",
+                border: "rgb(200, 180, 235)",
+            },
+            border: "rgb(180, 160, 215)",
+        },
+        ...GLOBALS_DATAS,
+        // /kill eyes
+        */
     },
     etheral: {
         //dark
