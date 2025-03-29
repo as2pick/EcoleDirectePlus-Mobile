c97317cb972ac77ca22536194a3344df9d690342
as2pick <vooxify.public@outlook.fr>
2025-03-29 10:16:56 +0100

    update routesNames

--- src/router/config/routesNames.js
+++ src/router/config/routesNames.js
@@ -1,2 +1,20 @@
-export const routesNames = {};
+export const routesNames = {
+    auth: {
+        login: "auth_login",
+        privacyPolicy: "auth_privacy_pol",
+    },
+    client: {
+        grades: "client_grades",
+        homeworks: "client_homeworks",
+        home: "client_home",
+        timetable: "client_timetable",
+        messaging: "client_messaging",
+    },
+    settings: {},
+    navigators: {
+        splash: "navigators_splash",
+        root: "navigators_root",
+        authentification: "navigators_authentification",
+    },
+};
 
