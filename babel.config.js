4208fdf74202151d93a656c3fdfda940b1bbf5ae
as2pick <vooxify.public@outlook.fr>
2025-05-04 22:01:44 +0200

    !!! MIGRATION !!! - update ALL dependencies (all this manually fucking) to EXPO 53 btw 52

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,11 @@
-module.exports = {
-    presets: ["module:@react-native/babel-preset"],
-    plugins: ["react-native-reanimated/plugin"],
-};
+module.exports = function (api) {
+    api.cache(true);
 
+    return {
+        presets: ["babel-preset-expo"],
+        plugins: [
+            "@babel/plugin-transform-class-static-block",
+            "react-native-reanimated/plugin",
+        ],
+    };
+};
