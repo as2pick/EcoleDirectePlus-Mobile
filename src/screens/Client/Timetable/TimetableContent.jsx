3df93a0e253e5097270873f9495166201f1f83ac
as2pick <vooxify.public@outlook.fr>
2025-09-14 11:41:58 +0200

    remove debug tools

--- src/screens/Client/Timetable/TimetableContent.jsx
+++ src/screens/Client/Timetable/TimetableContent.jsx
@@ -63,7 +63,6 @@ export default function TimetableContent() {
                         setLoading(false);
                     });
             } else {
-                console.log("Already exist in strorage");
             }
         }, [userAccesToken, sortedTimetableData])
     );
