b044edc48a0840eae5e380dae5988bd1e71cbb1e
as2pick <vooxify.public@outlook.fr>
2025-09-15 21:16:00 +0200

    add objectsEqual  in json utils

--- src/utils/json.js
+++ src/utils/json.js
@@ -27,9 +27,37 @@ export function isJsonObject(value) {
         return false;
     }
 }
+
+export function objectsEqual(obj1, obj2) {
+    if (!_.isPlainObject(obj1) || !_.isPlainObject(obj2)) {
+        return false;
+    }
+
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+
+    return keys1.every((key) => {
+        const val1 = obj1[key];
+        const val2 = obj2[key];
+
+        if (_.isPlainObject(val1) && _.isPlainObject(val2)) {
+            return objectsEqual(val1, val2);
+        } else if (Array.isArray(val1) && Array.isArray(val2)) {
+            return arraysEqual(val1, val2);
+        } else {
+            return val1 === val2;
+        }
+    });
+}
+
 export function arraysEqual(arr1, arr2) {
     if (arr1.length !== arr2.length) return false;
-
-    return arr1.every((value, index) => value === arr2[index]);
+    const set1 = new Set(arr1);
+    const set2 = new Set(arr2);
+    return [...set1].every((value) => set2.has(value));
 }
 
