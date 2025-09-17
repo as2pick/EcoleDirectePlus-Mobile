27759069315b0c0190fea59990ad95eb182a73d4
as2pick <vooxify.public@outlook.fr>
2025-09-17 09:08:37 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useEffect } from "react";
 import RootProviders from "./provider";
 import AuthNavigator from "./router/AuthNavigator";
-import authService from "./services/login/authService";
 
 export default function App() {
-    useEffect(() => {
-        authService.deleteStoredApiDatas();
-    }, []);
+    // useEffect(() => {
+    //     authService.deleteStoredApiDatas();
+    // }, []);
     return (
         <RootProviders>
             <AuthNavigator />
