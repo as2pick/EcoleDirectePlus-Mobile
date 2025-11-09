5f223818588434cc40eeed829a70c17f756d4db8
Lucilus <lucas.mage@proton.me>
2025-11-09 18:48:15 +0100

    add new colors in opulent theme

--- src/themes/themes.js
+++ src/themes/themes.js
@@ -7,31 +7,41 @@ export const THEMES = {
         colors: {
             ...NavigationDefaultTheme.colors,
             background: {
-                gradient: ["rgba(169, 169, 255, 1)", "rgba(227, 227, 255, 1)"], // Rouge profond en dégradé
-                login: "rgba(255, 255, 255, 1)"
+                //gradient: ["rgba(169, 169, 255, 1)", "rgba(230, 230, 255, 1)"], // App backround gradient
+                gradient: ["rgba(222, 222, 250, 1)", "rgba(222, 222, 250, 1)"],
+                login: "rgba(230, 230, 255, 1)", // Login screen background
             },
             bg: {
-                bg1: "rgba(35, 35, 207, 1)", // Rouge vif
-                bg2: "rgba(255, 255, 255, 1)", // Rouge foncé
-                bg3: "rgba(26, 26, 151, 1)", // Rouge clair
-                bg4: "rgba(148, 148, 255, 1)", // Rouge sombre
-                bg5: "rgba(148, 148, 255, 1)", // Rose pâle
-                bg6: "rgba(35, 35, 207, 1)", // Rouge très foncé
+                bg1: "rgba(35, 35, 207, 1)", // 
+                bg2: "rgba(255, 255, 255, 1)", // 
+                bg3: "rgba(26, 26, 151, 1)", // Background Modal
+                bg4: "rgba(148, 148, 255, 1)", // 
+                bg5: "rgba(148, 148, 255, 1)", // Useless ?
+                bg6: "rgba(35, 35, 207, 1)", // Useless ?
             },
             txt: {
-                txt1: "rgb(0, 0, 0)", // Blanc pour contraste
-                txt2: "rgba(26, 26, 151, 1)", // Rose clair
-                txt3: "rgba(35, 35, 207, 1)", // Rose moyen
+                contrast: "rgba(0, 0, 0, 1)", // Contrast text
+                secondary: "rgba(0, 0, 153, 1)", // Secondary color text
+                primary: "rgba(35, 35, 207, 1)", // Standard text
+                muted: "rgba(35, 35, 207, 0.5)", // Muted color
             },
             navbar: {
-                icons: "rgba(0, 0, 0, 1)", // Icônes blanches
                 active_icon: "rgba(126, 126, 255, 1)",
-                active_icon_bg: "rgba(96, 96, 192, 1)", // Rouge actif
-                icons_bg: "rgba(126, 126, 255, 1)", // Rouge foncé pour fond d'icônes
-                background: "rgba(148, 148, 255, 1)", // Rouge très foncé pour la navbar
-                border: "rgba(126, 126, 255, 1)", // Rouge pour bordure
+                active_icon_bg: "rgba(96, 96, 192, 1)",
+                icons_bg: "rgba(126, 126, 255, 1)",
+                background: "rgba(148, 148, 255, 1)",
+                border: "rgba(126, 126, 255, 1)",
             },
-            border: "rgba(26, 26, 151, 1)", // Rouge clair pour les bordures générales
+            border: "rgba(26, 26, 151, 1)",
+            icons: "rgba(0, 0, 0, 1)",
+            error: "rgb(240, 90, 90)",
+            fond: ["rgba(222, 222, 250, 1)", "rgba(222, 222, 250, 1)"],
+            pastel: "rgb(222, 222, 250)",
+            secondary: "rgb(180, 180, 253)",
+            main: "rgb(119, 119, 247)",
+            accent: "rgb(62, 62, 221)",
+            contrast: "rgb(0, 0, 0)",
+            case: "white",
         },
         fonts: {
             bold: { fontFamily: "Luciole-Regular", fontWeight: "600" },
@@ -49,7 +59,7 @@ export const THEMES = {
             // background: "rgb(18, 18, 138)", // default key
             background: {
                 gradient: ["rgb(18, 18, 48)", "rgb(43, 43, 63)"], // +20 or +30
-                login: "rgb(19, 19, 34)"
+                login: "rgb(19, 19, 34)", // Fond de l'écran de login
             },
             bg: {
                 bg1: "rgb(25, 25, 56)",
@@ -60,12 +70,12 @@ export const THEMES = {
                 bg6: "rgb(64, 64, 130)",
             },
             txt: {
-                txt1: "rgb(255, 255, 255)",
-                txt2: "rgb(109, 106, 251)",
-                txt3: "rgb(180, 180, 240)",
+                contrast: "rgb(255, 255, 255)",
+                secondary: "rgb(109, 106, 251)",
+                primary: "rgb(180, 180, 240)",
+                muted: "rgba(180, 180, 240, 0.5)"
             },
             navbar: {
-                icons: "rgb(255, 255, 255)",
                 active_icon_bg: "rgb(34, 34, 52)",
                 icons_bg: "rgb(17, 17, 38)",
                 background: "rgb(12, 12, 32)",
@@ -73,6 +83,13 @@ export const THEMES = {
             },
 
             border: "rgb(92, 113, 250)",
+            icons: "rgb(255, 255, 255)",
+            fond: "rgb(255, 255, 255)",
+            pastel: "rgb(222, 222, 250)",
+            secondary: "rgb(180, 180, 253)",
+            main: "rgb(119, 119, 247)",
+            accent: "rgb(62, 62, 221)",
+            case: "black",
         },
         fonts: {
             bold: { fontFamily: "Luciole-Regular", fontWeight: "600" },
