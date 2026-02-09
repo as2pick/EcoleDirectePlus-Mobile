ab24f4e8a3387e4308cadf0166094fc9c0018565
As de Pique <as2pick.card@outlook.fr>
2026-02-09 19:41:41 +0100

    clean console log

--- src/screens/Client/Homeworks/HomeworksContent.jsx
+++ src/screens/Client/Homeworks/HomeworksContent.jsx
@@ -181,7 +181,6 @@ export default function HomeworksContent() {
                 )
             );
         }
-        console.log(merged);
         setSortedHomeworksData(merged);
     }, [customHomeworksData]);
 
