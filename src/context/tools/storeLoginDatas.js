13a1cd9c7e0a05841ff3d35393e64636ecd08f69
As de Pique <as2pick.card@outlook.fr>
2026-04-17 16:03:29 +0200

    start context migration (to zustand)

--- src/context/tools/storeLoginDatas.js
+++ src/context/tools/storeLoginDatas.js
@@ -1,18 +1,28 @@
 // setUserData.js
 import { API } from "../../constants/api/api";
 import authService from "../../services/login/authService";
+import { getUseUsertmp, setUseUsertmp } from "../../store/useUser";
 
 export default function storeDatas({
     data,
     token,
-    setGlobalUserData,
-    setUserAccesToken,
-    setIsConnected,
+    setGlobalUserData, // r
+    setUserAccesToken, // r
+    setIsConnected, // r
 }) {
-    setGlobalUserData(data);
-    setUserAccesToken(token);
+    setUseUsertmp({
+        globalUserData: data,
+        userAccesToken: token,
+        isConnected: true,
+    });
+
+    setGlobalUserData(data); // r
+    setUserAccesToken(token); // r
     API.USER_ID = data.id;
-    setIsConnected(true);
+    setIsConnected(true); // r
     authService.storeUserData(data);
+
+    // to get the live value of globalUserData you need to do getUseUsertmp().globalUserData
+    console.log(getUseUsertmp().userAccesToken);
 }
 
