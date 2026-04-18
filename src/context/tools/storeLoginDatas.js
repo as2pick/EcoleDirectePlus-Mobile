c2ecf231b077befcd6dcd00893bc36c0f575fa88
As de Pique <as2pick.card@outlook.fr>
2026-04-18 10:05:05 +0200

    revert storeDatas

--- src/context/tools/storeLoginDatas.js
+++ src/context/tools/storeLoginDatas.js
@@ -1,28 +1,17 @@
 // setUserData.js
 import { API } from "../../constants/api/api";
 import authService from "../../services/login/authService";
-import { getUseUsertmp, setUseUsertmp } from "../../store/useUser";
 
 export default function storeDatas({
     data,
     token,
-    setGlobalUserData, // r
-    setUserAccesToken, // r
-    setIsConnected, // r
+    setGlobalUserData,
+    setUserAccesToken,
+    setIsConnected,
 }) {
-    setUseUsertmp({
-        globalUserData: data,
-        userAccesToken: token,
-        isConnected: true,
-    });
-
-    setGlobalUserData(data); // r
-    setUserAccesToken(token); // r
+    setGlobalUserData(data);
+    setUserAccesToken(token);
     API.USER_ID = data.id;
-    setIsConnected(true); // r
+    setIsConnected(true);
     authService.storeUserData(data);
-
-    // to get the live value of globalUserData you need to do getUseUsertmp().globalUserData
-    console.log(getUseUsertmp().userAccesToken);
 }
-
