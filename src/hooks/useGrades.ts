32f21004406a4948c57f0480e68eb2f9a96ace62
Lucilus <lucas.mage@proton.me>
2026-04-30 13:36:44 +0200

    add grade and timetable hooks

---
+++ src/hooks/useGrades.ts
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+import gradesResolver from "../resolver/grades";
+
+export function useGrades(token: string) {
+    return useQuery({
+        queryKey: ["grades"],
+        queryFn: () => gradesResolver({ token }),
+        enabled: Boolean(token),
+    });
+}
---
+++ src/hooks/useTimetable.ts
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+import timetableResolver from "../resolver/timetable";
+
+export function useTimetable(token: string, offset: number = 0) {
+    return useQuery({
+        queryKey: ["timetable", offset],
+        queryFn: () => timetableResolver({ token, offset }),
+        enabled: Boolean(token),
+    });
+}
