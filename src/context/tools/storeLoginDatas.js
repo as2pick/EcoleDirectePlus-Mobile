a02a9c7e4b6b142822e3c92003a3d18b36b18ce4
Lucilus <lucas.mage@proton.me>
2026-05-25 14:07:35 +0200

    fix data mapping issue

--- src/context/tools/storeLoginDatas.js
+++ src/context/tools/storeLoginDatas.js
@@ -5,7 +5,25 @@ export default function storeDatas({
     data,
     token,
 }) {
-    useUserStore.getState().setProfile(data);
+    if (!data) return;
+
+    const isAlreadyFormatted = data && 'name' in data;
+
+    const formattedProfile = isAlreadyFormatted ? data : {
+        id: data.id,
+        name: data.prenom,
+        surname: data.nom,
+        sex: data.profile?.sexe || "",
+        phone: data.profile?.telPortable || "",
+        email: data.email || "",
+        schoolName: data.nomEtablissement || "",
+        class: {
+            libelle: data.profile?.classe?.libelle || "",
+            code: data.profile?.classe?.code || "",
+        },
+    };
+
+    useUserStore.getState().setProfile(formattedProfile);
     useUserStore.getState().setToken(token);
     API.USER_ID = data.id;
 }
