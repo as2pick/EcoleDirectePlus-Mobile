460247fa3560f2255357f495f02b121c43fd906a
As de Pique <as2pick.card@outlook.fr>
2026-07-14 16:24:54 +0200

    add utils functions

--- src/utils/time.js
+++ src/utils/time.js
@@ -50,4 +50,59 @@ export const getTimeInterval = (
 
     return [intervalDays, intervalHours, intervalMinutes];
 };
+export const convertTimeHoursMinToMinutes = (...time) => {
+    return time.map((t) => {
+        const [h, m] = t.split(":").map(Number);
+        return h * 60 + m;
+    });
+};
 
+export const isBefore = (t1, t2) => {
+    const [minutesT1, minutesT2] = convertTimeHoursMinToMinutes(t1, t2);
+    if (minutesT1 <= minutesT2) return true;
+    else return false;
+};
+export const isAfter = (t1, t2) => {
+    const [minutesT1, minutesT2] = convertTimeHoursMinToMinutes(t1, t2);
+    if (minutesT1 >= minutesT2) return true;
+    else return false;
+};
+
+export const isInInterval = (time, timeStart, timeEnd) => {
+    if (isAfter(time, timeStart) && isBefore(time, timeEnd)) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+export const formatDuration = ([days, hours, minutes], pattern = "normal") => {
+    if (pattern === "short") {
+        if (days === 0 && hours === 0) {
+            return `${minutes}min`;
+        }
+        if (days > 0) {
+            if (days > 4) {
+                return `+${days}j`;
+            }
+            return `${days}j${hours}h${minutes}`;
+        }
+        if (hours > 4) {
+            return `+${hours}h`;
+        }
+        return `${hours}h${minutes}`;
+    }
+    if (days === 0 && hours === 0) {
+        return `${minutes} min`;
+    }
+    if (days > 0) {
+        if (days > 4) {
+            return `plus de ${days} jours`;
+        }
+        return `${days}j ${hours}h ${minutes}min`;
+    }
+    if (hours > 4) {
+        return `plus de ${hours} heures`;
+    }
+    return `${hours}h ${minutes}min`;
+};
